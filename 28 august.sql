3e28a1fb1e1c8df1e88ba9c367a0c55756ea1eba
Shyam <shyamvidyadharpatil1234@gmail.com>
2025-08-29 13:39:58 +0530

    indexing

---
+++ 28 august.sql
@@ -0,0 +1,25 @@
+use microsoft
+select * from employee
+select * from employee where city = 'delhi'
+--NON CLUSTERED INDEX
+create index in_city on employee(city)
+
+create nonclustered index Inx_age on employee(age)
+
+select * from employee where age = 45
+
+drop index employee.in_city
+drop index employee.inx_age
+
+alter table employee alter column id int not null
+alter table employee add constraint pri_id Primary key(id)
+
+alter table employee drop constraint pri_id
+
+create clustered index inx_name on employee(name)
+
+alter table employee add constraint pri_id Primary key nonclustered(id)
+
+alter table employee drop constraint pri_id
+
+drop index employee.inx_name
